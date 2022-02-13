a302990e1eff39bde7eb8f29352c66b9c024d59b
Dave <61114342+DavesCodeMusings@users.noreply.github.com>
2022-02-13 14:01:41 -0600

    Update esp32-temperature.py

--- solutions/esp32-temperature.py
+++ solutions/esp32-temperature.py
@@ -1,4 +1,4 @@
-# Read ESP32 temperature and output to built-in display.
+# Read ESP32 temperature and output to built-in display along with Hello World!
 
 from machine import Pin, SoftI2C
 from ssd1306 import SSD1306_I2C
@@ -8,7 +8,9 @@ from esp32 import raw_temperature
 i2c = SoftI2C(scl=Pin(4), sda=Pin(5))
 oled = SSD1306_I2C(128, 64, i2c)
 
+oled.text("Hello World!", 0, 0)
+
 esp32_temp = raw_temperature()
-message = "CPU temp: {:d} F".format(esp32_temp)
-oled.text(message, 0, 0)
+message = "ESP32 temp: {:d}F".format(esp32_temp)
+oled.text(message, 0, 10)  # Start on pixel row 10 to leave some space between text above.
 oled.show()
